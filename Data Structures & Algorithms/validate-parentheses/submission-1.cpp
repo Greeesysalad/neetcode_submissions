269828c9ffc5755b5d01c8a12718472ac6b0d058
Ted Yao <yaoted00@gmail.com>
2026-06-15 17:19:19 -0600

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stk;
+
+        unordered_map<char, char> brackets;
+        brackets[']'] = '[';
+        brackets['}'] = '{';
+        brackets[')'] = '(';
+
+        for(char ele: s){
+            if(brackets.find(ele) == brackets.end()){
+                stk.push(ele);
+                continue;
+            }
+
+            if(stk.empty() == false && brackets[ele] == stk.top()){
+                stk.pop();
+                continue;
+            };
+
+            return false;            
+        };  
+
+        if(stk.empty() == true){return true;}
+        return false;
+    }
+};
