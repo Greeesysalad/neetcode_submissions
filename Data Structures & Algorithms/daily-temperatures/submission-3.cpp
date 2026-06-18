ed3e5124780689a3a40cbc3280c57fdd8b1a5cdf
Ted Yao <yaoted00@gmail.com>
2026-06-17 20:14:02 -0600

    Add: daily-temperatures - submission-3

---
+++ Data Structures & Algorithms/daily-temperatures/submission-3.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        vector<int> ans(temperatures.size(),0);
+        stack<int> indices;
+        indices.push(0);
+
+        for(int i = 1; i< temperatures.size(); i++){
+            
+            while(temperatures[i] > temperatures[indices.top()]){
+                ans[indices.top()] = i - indices.top();
+                indices.pop();
+
+                if(indices.empty()){break;}
+            }
+            indices.push(i);
+        }
+
+        return ans;
+    }
+};
